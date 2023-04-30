56b2c05be64be57bb370d2d3ec03a76119b40279
Chris Weed <chris@cjweed.com>
2023-04-30 10:07:16 -0500

    Initial auth, not working

---
+++ src/client/SignupPage.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+import { SignupForm } from '@wasp/auth/forms/Signup'
+
+const SignupPage = () => {
+  return (
+    <>
+      <SignupForm />
+      <br />
+      <span>
+        I already have an account (<Link to="/login">go to login</Link>).
+      </span>
+    </>
+  )
+}
+
+export default SignupPage
